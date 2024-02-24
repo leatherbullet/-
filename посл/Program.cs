ae25314a4d094d1f4ba869cdc13bf78d825e9398
79612 <79612@DESKTOP-O1P5TC5>
2024-02-24 10:43:50 +0300

    Добавьте файлы проекта.

---
+++ посл/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace посл
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // for т.к. задать параметры работы цикла
+
+            int minValue = 5, maxValue = 103, numberStep = 7;
+
+            for (int i = minValue; i <= maxValue; i+=numberStep)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
